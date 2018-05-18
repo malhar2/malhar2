84c4e139a69180dba66f52afd1f203d5a16ece6d
malhar2_gitdesktop <malhar.harlon@0hio.net>
2018-05-18 15:58:51 +0200

    *

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formspree.io/aveenr99@gmail.coma",
+                url: "https://formspree.io/aveenr99@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
